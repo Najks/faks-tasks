e18f227e154862d8a8059903273b0940cc4a8aad
nik damis <nik.damis@gmail.com>
2026-02-09 17:46:28 +0100

    added task_status table and CRUD

---
+++ app/Models/TaskStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskStatus extends Model
+{
+    use hasFactory;
+    protected $table = 'task_status';
+    protected $fillable = ["status"];
+
+
+}
