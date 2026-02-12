de8a18456959ecac43609b0e6db573d6df5ff17c
nik damis <nik.damis@gmail.com>
2026-02-12 20:49:52 +0100

    refactor API routes for authentication, added all routes.

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource("status", \App\Http\Controllers\TaskStatusController::class);
+Route::apiResource("tasks", \App\Http\Controllers\TaskController::class);
+Route::apiResource("subjects", \App\Http\Controllers\SubjectController::class);
 
 // login routes
-Route::post("login", [AuthController::class, "login"]);
-Route::post("register", [AuthController::class, "register"]);
+Route::prefix("auth")->name("auth.")->group(function () {
+    Route::post("login", [AuthController::class, "login"]);
+    Route::post("register", [AuthController::class, "register"]);
 
-Route::middleware("auth:sanctum")->group(function () {
-    Route::post("logout", [AuthController::class, "logout"]);
-    Route::get("me", [AuthController::class, "me"]);
+    Route::middleware("auth:sanctum")->group(function () {
+        Route::post("logout", [AuthController::class, "logout"]);
+        Route::get("me", [AuthController::class, "me"]);
+    });
 });
